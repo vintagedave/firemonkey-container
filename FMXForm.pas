d3be7dc46eee6a566c10cf5f09ced010b433cc64
vintagedave <vintagedave@f089544c-63f3-213f-9ebe-9fe7c9b86bcb>
2013-07-17 17:11:23 +0000

    Component (runtime and designtime packages) and an example project

---
+++ FMXForm.pas
@@ -0,0 +1,40 @@
+unit FMXForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Rtti, System.Classes,
+  System.Variants, FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs,
+  FMX.StdCtrls, FMX.Effects, FMX.Colors, FMX.TabControl, FMX.Menus,
+  FMX.Filter.Effects, FMX.Edit;
+
+type
+  TFireMonkeyForm = class(TForm)
+    Button1: TButton;
+    GroupBox1: TGroupBox;
+    ShadowEffect1: TShadowEffect;
+    Button2: TButton;
+    ReflectionEffect1: TReflectionEffect;
+    StyleBook2: TStyleBook;
+    Edit1: TEdit;
+    GlowEffect1: TGlowEffect;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    AlphaTrackBar1: TAlphaTrackBar;
+    TabItem2: TTabItem;
+    Switch1: TSwitch;
+    AniIndicator1: TAniIndicator;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FireMonkeyForm: TFireMonkeyForm;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ VCLForm.dfm
@@ -0,0 +1,37 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'VCL form'
+  ClientHeight = 412
+  ClientWidth = 557
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Panel1: TPanel
+    Left = 16
+    Top = 16
+    Width = 521
+    Height = 377
+    BevelInner = bvLowered
+    Caption = 'Panel1'
+    TabOrder = 0
+    object FireMonkeyContainer1: TFireMonkeyContainer
+      Left = 2
+      Top = 2
+      Width = 517
+      Height = 373
+      FireMonkeyForm = FireMonkeyForm.Owner
+      Align = alClient
+      ExplicitLeft = 24
+      ExplicitTop = 48
+      ExplicitWidth = 497
+      ExplicitHeight = 289
+    end
+  end
+end
