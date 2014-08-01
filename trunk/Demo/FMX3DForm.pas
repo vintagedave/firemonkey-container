9054ba18e63289743b567362e0101aec740f46c5
vintagedave <vintagedave@f089544c-63f3-213f-9ebe-9fe7c9b86bcb>
2014-08-01 11:00:12 +0000

    Demo tweaks, bug fixes (XE6 throws an exception 'Bitmap size too big' when using a TLayer3D, even in a standalone FMX app)

--- trunk/Demo/FMX3DForm.pas
+++ trunk/Demo/FMX3DForm.pas
@@ -5,21 +5,18 @@ interface
 uses
   System.SysUtils, System.Types, System.Variants, System.UITypes,
   System.Classes, FMX.Types, FMX.Dialogs, FMX.Types3D, FMX.Forms,
-  FMX.Forms3D, FMX.Controls3D, FMX.Objects3D, FMX.StdCtrls, FMX.Controls, FMX.Layers3D, FMX.Ani,
+  FMX.Forms3D, FMX.Controls3D, FMX.Objects3D, FMX.StdCtrls, FMX.Controls, FMX.Ani,
   FMX.MaterialSources;
 
 type
   TFormExample3D = class(TForm3D)
     Camera1: TCamera;
     RoundCube1: TRoundCube;
-    Layer3D1: TLayer3D;
-    Button1: TButton;
     LightMaterialSource1: TLightMaterialSource;
     animRotateX: TFloatAnimation;
     animRotateY: TFloatAnimation;
     Light1: TLight;
     Light2: TLight;
-    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -32,10 +29,5 @@ var
 implementation
 
 {$R *.fmx}
-
-procedure TFormExample3D.Button1Click(Sender: TObject);
-begin
-  MessageDlg('Hello from ' + (Sender as TComponent).Name, TMsgDlgType.mtInformation, [TMsgDlgBtn.mbOK], 0);
-end;
 
 end.
